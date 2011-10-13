e9b33ec2a4b4d81187e298865e28fad4b19d24ac
Jack Senechal <jack@jacksenechal.com>
2011-10-13 03:11:55 -0700

    Closes #1, closes #2;
    - added an options page that lets the user edit:
      - international regex
      - domestic regex
      - link type (callto vs skype)
    - link type now defaults to skype
    - added a license

--- contentscript.js
+++ contentscript.js
@@ -1,18 +1,35 @@
 /*
- * Copyright (c) 2010 The Chromium Authors. All rights reserved.  Use of this
+ * Copyright (c) 2011 Jack Senechal. All rights reserved.  Use of this
  * source code is governed by a BSD-style license that can be found in the
  * LICENSE file.
  */
-var regex = /\b\(?(\d{3})\)?[. -](\d{3})[. -](\d{4})\b/;
-var replacement = '<a href="callto:+1$1$2$3">$&</a>';
 
-// Test the text of the body element against our regular expression.
-if (regex.test(document.body.innerText)) {
-  //console.log("replacing text...");
-  $(document).find(':not(textarea)').replaceText( regex, replacement );
-  //console.log("done. what's next, boss?");
-  // The regular expression produced a match, so notify the background page.
-  chrome.extension.sendRequest({}, function(response) {});
-} else {
-  // No match was found.
-}
+// load options
+chrome.extension.sendRequest({action: 'options'}, function(response) {
+  // the localStorage mechanism converts the regex to a string, so we have to convert it back
+  var stripper = /^\/|\/$/g;
+  var intlRegex = RegExp(response.options.intlRegex.replace(stripper, ''));
+  var homeRegex = RegExp(response.options.homeRegex.replace(stripper, ''));
+
+  var linkType = response.options.linkType;
+  var intlReplacement = '<a href="'+linkType+response.options.intlReplacement+'">$&</a>';
+  var homeReplacement = '<a href="'+linkType+response.options.homeReplacement+'">$&</a>';
+
+  var found = false;
+
+  // Test the text of the body element against our international regular expression.
+  if (intlRegex.test(document.body.innerText)) {
+    $(document).find(':not(textarea)').replaceText( intlRegex, intlReplacement );
+    found = true;
+  }
+  // Test the text of the body element against our home regular expression.
+  if (homeRegex.test(document.body.innerText)) {
+    $(document).find(':not(textarea)').replaceText( homeRegex, homeReplacement );
+    found = true;
+  }
+  if (found) {
+    // Notify the background page to update the page icon
+    chrome.extension.sendRequest({action: 'showPageAction'}, function() {});
+  }
+});
+
